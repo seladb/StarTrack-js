dd812db4ac99baa0f4e9beb324599ed21fe19a9b
seladb <pcapplusplus@gmail.com>
2019-12-20 02:12:09 -0800

    Change functions to classes

--- src/containers/RepoDetails.js
+++ src/containers/RepoDetails.js
@@ -1,31 +1,34 @@
 import React from 'react'
 import { Container, Row, InputGroup, FormControl, Button } from 'react-bootstrap/'
 
-function RepoDetails() {
+class RepoDetails extends React.Component {
 
-  return (
-    <Container className="mt-5 mb-5">
-      <Row>
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text id="basic-addon1">Repo Details:</InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl
-            placeholder="Username"
-            aria-label="Username"
-          />
-          <InputGroup.Prepend>
-            <InputGroup.Text id="basic-addon1">/</InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl
-            placeholder="Repo name"
-            aria-label="Repo name"
-          />
-          <Button type="button">Go</Button>
-        </InputGroup>
-      </Row>
-    </Container>
-  )
+  render() {
+    return (
+      <Container className="mt-5 mb-5">
+        <Row>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text id="basic-addon1">Repo Details:</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              placeholder="Username"
+              aria-label="Username"
+            />
+            <InputGroup.Prepend>
+              <InputGroup.Text id="basic-addon1">/</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              placeholder="Repo name"
+              aria-label="Repo name"
+            />
+            <Button type="button" onClick="">Go</Button>
+          </InputGroup>
+        </Row>
+      </Container>
+    )
+  }
+  
 }
 
 export default RepoDetails
--- src/containers/TopNav.js
+++ src/containers/TopNav.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import { Nav, Navbar, Button } from 'react-bootstrap/'
 
-function TopNav() {
-  return (
-    <Navbar bg="primary" variant="dark">
-      <Navbar.Brand href="#home">
-        <img
-          alt=""
-          src="/star-icon.png"
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        />{' '}
-        StarTrack
-      </Navbar.Brand>
-      <Nav className="mr-auto"/>
-      <Button>GitHub Authentication</Button>
-    </Navbar>
-  )
+class TopNav extends React.Component {
+  
+  render() {
+    return (
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="/star-icon.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+          StarTrack
+        </Navbar.Brand>
+        <Nav className="mr-auto"/>
+        <Button>GitHub Authentication</Button>
+      </Navbar>
+    )
+  }
 }
 
 export default TopNav;
