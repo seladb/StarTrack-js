290d98165852fb09e1820d9346a9282cc3b626ad
seladb <pcapplusplus@gmail.com>
2020-01-13 22:25:03 -0800

    Fix access token validation in github auth form

--- src/containers/GitHubAuth.js
+++ src/containers/GitHubAuth.js
@@ -5,45 +5,50 @@ import axios from 'axios'
 class GitHubAuth extends React.Component {
 
   state = {
-    formValidated: false
+    tokenValid: true
   }
 
   constructor(props) {
     super(props);
     this.inputToken = React.createRef(); 
- }
+  }
 
-  handleLogin = event => {
+  handleLoginClick = event => {
     event.preventDefault();
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.stopPropagation();
-    }
-    else {
-      this.setState({
-        formValidated: true
-      });
-      console.log(this.inputToken.current.value);
-      axios.get(`https://api.github.com`, { headers: { 'Authorization': 'token ' + this.inputToken.current.value} })
-        .then(response => {
-          window.sessionStorage.setItem('access_token', this.inputToken.current.value);
+
+    axios.get(`https://api.github.com`, { headers: { 'Authorization': 'token ' + this.inputToken.current.value} })
+      .then( () => {
+        window.sessionStorage.setItem('access_token', this.inputToken.current.value);
+        this.setState({
+          tokenValid: true
+        }, () => {
           this.props.handleLoginSuccess();
         })
-        .catch(error => {
-          console.log(error.response)
-      });
-    }
+      })
+      .catch(error => {
+        this.setState({
+          tokenValid: false
+        })
+      })
+  }
+
+  handleCloseClick() {
+    this.setState({
+      tokenValid: true
+    }, () => {
+      this.props.handleClose();
+    })
   }
 
   render() {
     return (
-      <Modal show={this.props.show} onHide={this.props.handleClose}>
-        <Form noValidate validated={this.state.formValidated} onSubmit={this.handleLogin}>
+      <Modal show={this.props.show} onHide={this.props.handleCloseClick}>
+        <Form onSubmit={this.handleLoginClick}>
           <Modal.Header closeButton>
             <Modal.Title>GitHub Authentication</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <p>GitHub API <a href="https://developer.github.com/v3/#rate-limiting">rate limiter</a> makes it 
+            <p>GitHub API <a target="_blank" href="https://developer.github.com/v3/#rate-limiting">rate limiter</a> makes it 
               impossible to collect stargazer data on repos with more than 3000 stars without GitHub authentication.
             </p>
             <p>If you'd like to view stargazer data for this repo, please provide your GitHub auth details.</p>
@@ -51,10 +56,10 @@ class GitHubAuth extends React.Component {
               so the credentials are only used to send authenticated requests to GitHub API.
             </p>
             <Form.Group controlId="githubAuthenticationForm">
-              <Form.Label>GitHub access token (generate one <a href="https://github.com/settings/tokens">here</a>)</Form.Label>
-              <Form.Control ref={this.inputToken} type="text" placeholder="fc516773214acf13d10f856c6b80037999da4fd3" required/>
+              <Form.Label>GitHub access token (generate one <a target="_blank" href="https://github.com/settings/tokens">here</a>)</Form.Label>
+              <Form.Control ref={this.inputToken} type="text" placeholder="fc516773214acf13d10f856c6b80037999da4fd3" isInvalid={!this.state.tokenValid} required/>
               <Form.Control.Feedback type="invalid">
-                Please choose a username.
+                Access token is invalid.
               </Form.Control.Feedback>
               <Form.Text className="text-muted">
                 These credentials aren't stored in any server.
