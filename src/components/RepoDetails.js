4a57d2adf94697b2dc735aded3f51febf3c32af3
seladb <pcapplusplus@gmail.com>
2020-02-12 00:20:22 -0800

    Automatic parsing of GitHub URLs or repo names in RepoDetails

--- src/components/RepoDetails.js
+++ src/components/RepoDetails.js
@@ -1,8 +1,9 @@
 import React, { useRef } from 'react'
 import './RepoDetails.css'
-import { Container, Row, InputGroup, FormControl, Button, Spinner } from 'react-bootstrap/'
+import { Container, Row, InputGroup, FormControl, Button, Spinner, OverlayTrigger, Tooltip } from 'react-bootstrap/'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStopCircle } from '@fortawesome/free-solid-svg-icons';
+import gh from 'parse-github-url'
 
 const RepoDetails = (props) => {
 
@@ -19,18 +20,35 @@ const RepoDetails = (props) => {
     } 
   }
 
+  const handlePaste = (event) => {
+    let clipData = gh(event.clipboardData.getData('Text'));
+    if ((clipData.protocol === "https:" && clipData.hostname === "github.com" && clipData.name !== null && clipData.owner !== null) ||
+          (clipData.protocol === null && clipData.hostname === null && clipData.name !== null && clipData.owner !== null )) {
+      event.preventDefault();
+      userName.current.value = clipData.owner;
+      repoName.current.value = clipData.name
+    }
+  }
+
   return (
     <Container className="RepoDetails-container">
       <Row>
         <InputGroup>
           <InputGroup.Prepend>
-            <InputGroup.Text>Repo Details</InputGroup.Text>
+            <OverlayTrigger
+              placement="bottom"
+              delay={{ show: 100 }}
+              overlay={<Tooltip>Tip: you can paste any GitHub URL or string in the format of "username/repo"</Tooltip>}
+            >
+              <InputGroup.Text>Repo Details</InputGroup.Text>
+            </OverlayTrigger>
           </InputGroup.Prepend>
           <FormControl
             ref={userName}
             placeholder="Username"
             aria-label="Username"
             onKeyPress={handleKeyPress}
+            onPaste={handlePaste}
           />
           <InputGroup.Prepend>
             <InputGroup.Text>/</InputGroup.Text>
@@ -40,6 +58,7 @@ const RepoDetails = (props) => {
             placeholder="Repo name"
             aria-label="Repo name"
             onKeyPress={handleKeyPress}
+            onPaste={handlePaste}
           />
           { !props.loadInProgress ?
           <Button
