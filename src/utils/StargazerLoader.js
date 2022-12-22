5cf1fa0636d46aa8d62c350c887c5ba6a0733d88
JIMMY ZHAO <knightyzhao@gmail.com>
2022-12-22 18:38:32 -0500

    add color from 8 to 43

--- src/utils/StargazerLoader.js
+++ src/utils/StargazerLoader.js
@@ -1,18 +1,54 @@
 import gitHubUtils from "./GitHubUtils";
 import stargazerStats from "./StargazerStats";
 
-const colors = [
-  "#008FFB",
-  "#00E396",
-  "#FEB019",
-  "#FF4560",
-  "#775DD0",
-  "#F86624",
-  "#00B1F2",
-  "#5A2A27",
+// Refer to post https://stackoverflow.com/questions/10014271/generate-random-color-distinguishable-to-humans
+Colors = [
+  "#00ffff", // aqua
+  "#f0ffff", // azure
+  "#f5f5dc", // beige
+  "#000000", // black
+  "#0000ff", // blue
+  "#a52a2a", // brown
+  "#00ffff", // cyan
+  "#00008b", // darkblue
+  "#008b8b", // darkcyan
+  "#a9a9a9", // darkgrey
+  "#006400", // darkgreen
+  "#bdb76b", // darkkhaki
+  "#8b008b", // darkmagenta
+  "#556b2f", // darkolivegreen
+  "#ff8c00", // darkorange
+  "#9932cc", // darkorchid
+  "#8b0000", // darkred
+  "#e9967a", // darksalmon
+  "#9400d3", // darkviolet
+  "#ff00ff", // fuchsia
+  "#ffd700", // gold
+  "#008000", // green
+  "#4b0082", // indigo
+  "#f0e68c", // khaki
+  "#add8e6", // lightblue
+  "#e0ffff", // lightcyan
+  "#90ee90", // lightgreen
+  "#d3d3d3", // lightgrey
+  "#ffb6c1", // lightpink
+  "#ffffe0", // lightyellow
+  "#00ff00", // lime
+  "#ff00ff", // magenta
+  "#800000", // maroon
+  "#000080", // navy
+  "#808000", // olive
+  "#ffa500", // orange
+  "#ffc0cb", // pink
+  "#800080", // purple
+  "#800080", // violet
+  "#ff0000", // red
+  "#c0c0c0", // silver
+  "#ffffff", // white
+  "#ffff00"  // yellow
 ];
 
-export const maxReposAllowed = 8;
+export const maxReposAllowed = 43;
 
 class StargazerLoader {
   static colorIndex = -1;
@@ -40,17 +76,17 @@ class StargazerLoader {
     return {
       username: username,
       repo: repo,
-      color: colors[StargazerLoader.colorIndex],
+      color: Colors[StargazerLoader.colorIndex],
       stargazerData: stargazerData,
       stats: stargazerStats.calcStats(stargazerData),
       forecast:
         forecastProps !== null
           ? stargazerStats.calcForecast(
-              stargazerData,
-              forecastProps.daysBackwards,
-              forecastProps.daysForward,
-              forecastProps.numValues
-            )
+            stargazerData,
+            forecastProps.daysBackwards,
+            forecastProps.daysForward,
+            forecastProps.numValues
+          )
           : null,
     };
   }
