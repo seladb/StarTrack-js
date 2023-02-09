a3e07b74c26919c6af411fff1af10e4c8b25b96e
seladb <pcapplusplus@gmail.com>
2023-02-08 16:32:37 -0800

    Add GitHubUtils

---
+++ src/utils/GitHubUtils.test.ts
@@ -0,0 +1,287 @@
+import * as utils from "./GitHubUtils"
+import axios from "axios"
+
+describe("Test range", () => {
+  it("Returns a range starting from zero", () => {
+    expect(utils.range(5)).toStrictEqual([0, 1, 2, 3, 4])
+  })
+
+  it("Returns a range not starting from zero", () => {
+    expect(utils.range(4, 2)).toStrictEqual([2, 3, 4, 5])
+  })
+})
+
+describe("Test getRepoUrl", () => {
+  it("Get the right repo URL", () => {
+    expect(utils.getRepoUrl("seladb", "startrack-js")).toBe(
+      "https://github.com/seladb/startrack-js",
+    )
+  })
+})
+
+class StorageMock implements Storage {
+  private _storage
+  public length
+
+  constructor() {
+    this._storage = new Map<string, string>()
+    this.length = 0
+  }
+
+  getItem(key: string): string | null {
+    const val = this._storage.get(key)
+    return val ? val : null
+  }
+
+  setItem(key: string, value: string) {
+    this._storage.set(key, value)
+    this.length = this._storage.size
+  }
+
+  removeItem(key: string): void {
+    this._storage.delete(key)
+  }
+
+  clear(): void {
+    this._storage.clear()
+    this.length = 0
+  }
+
+  key(): string | null {
+    return null
+  }
+}
+
+describe("Test getStorage", () => {
+  it("Get from session storage if no token is stored", () => {
+    expect(utils.getStorage()).toStrictEqual(window.sessionStorage)
+  })
+
+  it("Get from session storage if token is stored in both", () => {
+    jest.spyOn(window.localStorage, "getItem")
+    window.localStorage.getItem = () => {
+      return "localStorage"
+    }
+
+    jest.spyOn(window.sessionStorage, "getItem")
+    window.sessionStorage.getItem = () => {
+      return "sessionStorage"
+    }
+
+    expect(utils.getStorage()).toStrictEqual(window.sessionStorage)
+  })
+
+  it("Get from local storage if token is stored in localStorage", () => {
+    jest.spyOn(window.localStorage, "getItem")
+    window.localStorage.getItem = () => {
+      return "localStorage"
+    }
+
+    expect(utils.getStorage()).toStrictEqual(window.localStorage)
+  })
+})
+
+describe("Test getStorageType", () => {
+  it("Get sessionStorage type", () => {
+    expect(utils.getStorageType()).toStrictEqual(utils.StorageType.SessionStorage)
+  })
+})
+
+describe("Test setStorageType", () => {
+  it("Set the correct type", () => {
+    expect(utils.setStorageType(utils.StorageType.LocalStorage)).toStrictEqual(window.localStorage)
+    expect(utils.setStorageType(utils.StorageType.SessionStorage)).toStrictEqual(
+      window.sessionStorage,
+    )
+  })
+})
+
+describe("Test getAccessToken", () => {
+  const storage = new StorageMock()
+
+  beforeEach(() => {
+    jest.spyOn(utils, "getStorage").mockReturnValue(storage)
+  })
+
+  it("No access token", () => {
+    expect(utils.getAccessToken()).toBeNull()
+  })
+
+  it("Get the correct access token", () => {
+    storage.setItem(utils.storageKey, "accessToken")
+    expect(utils.getAccessToken()).toStrictEqual("accessToken")
+  })
+})
+
+describe("Test removeAccessToken", () => {
+  const storage = new StorageMock()
+
+  beforeEach(() => {
+    jest.spyOn(utils, "getStorage").mockReturnValue(storage)
+  })
+
+  it("Remove access token when exists", () => {
+    storage.setItem(utils.storageKey, "accessToken")
+    utils.removeAccessToken()
+    expect(storage.getItem(utils.storageKey)).toBeNull()
+  })
+
+  it("Does nothing when access token doesn't exist", () => {
+    utils.removeAccessToken()
+    expect(storage.getItem(utils.storageKey)).toBeNull()
+  })
+})
+
+describe("Test prepareRequestHeaders", () => {
+  it("Returns the correct headers with access token", () => {
+    expect(utils.prepareRequestHeaders("accessToken")).toStrictEqual({
+      headers: {
+        Accept: "application/vnd.github.v3.star+json",
+        Authorization: "token accessToken",
+      },
+    })
+  })
+
+  it("Returns the correct headers without access token", () => {
+    expect(utils.prepareRequestHeaders(null)).toStrictEqual({
+      headers: {
+        Accept: "application/vnd.github.v3.star+json",
+        Authorization: "",
+      },
+    })
+  })
+})
+
+const fakeHeaders = {
+  headers: {
+    Accept: "accept",
+    Authorization: "token",
+  },
+}
+
+describe("Test validateAndStoreAccessToken", () => {
+  beforeEach(() => {
+    jest.spyOn(utils, "setStorageType").mockImplementation(() => {
+      return new StorageMock()
+    })
+    jest.spyOn(utils, "prepareRequestHeaders").mockReturnValue(fakeHeaders)
+  })
+
+  it("Validates an access token successfully", async () => {
+    jest.spyOn(axios, "get").mockImplementation(() => Promise.resolve())
+
+    const result = await utils.validateAndStoreAccessToken(
+      "accessToken",
+      utils.StorageType.LocalStorage,
+    )
+
+    expect(result).toBeTruthy()
+    expect(axios.get).toBeCalledWith("https://api.github.com/user", fakeHeaders)
+    expect(utils.setStorageType).toBeCalledWith(utils.StorageType.LocalStorage)
+  })
+
+  it("Fails when access token is not valid", async () => {
+    jest.spyOn(axios, "get").mockImplementation(() => Promise.reject())
+
+    const result = await utils.validateAndStoreAccessToken(
+      "accessToken",
+      utils.StorageType.SessionStorage,
+    )
+
+    expect(result).toBeFalsy()
+    expect(axios.get).toBeCalledWith("https://api.github.com/user", fakeHeaders)
+    expect(utils.setStorageType).not.toBeCalled()
+  })
+})
+
+describe("Test loadStarGazerPage", () => {
+  beforeEach(() => {
+    jest.spyOn(utils, "prepareRequestHeaders").mockReturnValue(fakeHeaders)
+  })
+
+  it("Loads a page successfully", async () => {
+    jest.spyOn(utils, "getAccessToken").mockReturnValue("accessToken")
+    jest.spyOn(axios, "get").mockImplementation(() => Promise.resolve())
+
+    await utils.loadStarGazerPage("user", "repo", 12)
+
+    expect(utils.prepareRequestHeaders).toBeCalledWith("accessToken")
+    expect(axios.get).toBeCalledWith(
+      "https://api.github.com/repos/user/repo/stargazers?per_page=100&page=12",
+      fakeHeaders,
+    )
+  })
+})
+
+describe("Test addStarData", () => {
+  it("Adds stars data correctly", () => {
+    const curStarCount = 1
+    const starData = [
+      {
+        x: "100",
+        y: curStarCount,
+      },
+    ]
+
+    const githubStarData = [
+      {
+        // eslint-disable-next-line camelcase
+        starred_at: "200",
+      },
+      {
+        // eslint-disable-next-line camelcase
+        starred_at: "300",
+      },
+    ]
+
+    const result = utils.addStarData(starData, curStarCount, githubStarData)
+
+    expect(result).toStrictEqual(curStarCount + githubStarData.length)
+    expect(starData).toStrictEqual([
+      {
+        x: "100",
+        y: curStarCount,
+      },
+      {
+        x: "200",
+        y: curStarCount + 1,
+      },
+      {
+        x: "300",
+        y: curStarCount + 2,
+      },
+    ])
+  })
+})
+
+describe("Test getLastStargazerPage", () => {
+  it("Returns 1 if link header doesn't exist", () => {
+    expect(utils.getLastStargazerPage()).toStrictEqual(1)
+  })
+
+  it("Throws an error if can't find 'last' value in link header", () => {
+    const linkHeader =
+      '<https://api.github.com/repositories/24911519/stargazers?per_page=100&page=2>; rel="next"'
+    expect(() => {
+      utils.getLastStargazerPage(linkHeader)
+    }).toThrow("Cannot find last stargazer page")
+  })
+
+  /* eslint quotes: ["error", "double", { "avoidEscape": true }] */
+  it.each([
+    ["", 1],
+    [
+      '<https://api.github.com/repositories/24911519/stargazers?per_page=100&page=2>; rel="next", <https://api.github.com/repositories/24911519/stargazers?per_page=100&page=22>; rel="last"',
+      22,
+    ],
+    [
+      '<https://api.github.com/repositories/24911519/stargazers?per_page=100&page=22>; rel="last"',
+      22,
+    ],
+  ])(
+    "Returns expected last page for link header",
+    (linkHeader: string, expectedLastPage: number) => {
+      expect(utils.getLastStargazerPage(linkHeader)).toStrictEqual(expectedLastPage)
+    },
+  )
+})
---
+++ src/utils/GitHubUtils.ts
@@ -0,0 +1,230 @@
+import axios, { AxiosResponse } from "axios"
+
+const stargazersURL =
+  "https://api.github.com/repos/{user}/{repo}/stargazers?per_page=100&page={page}"
+const validateAccessTokenURL = "https://api.github.com/user"
+const repoUrlTemplate = "https://github.com/{user}/{repo}"
+
+export const storageKey = "statrack_js_access_token"
+
+const maxSupportedPagesWithoutAccessToken = 30
+
+const batchSize = 5
+
+export enum StorageType {
+  LocalStorage = "local storage",
+  SessionStorage = "session storage",
+}
+
+interface GitHubStarData {
+  starred_at: string
+}
+
+interface InternalStarData {
+  x: string
+  y: number
+}
+
+type HandleProgressCallback = (progress: number) => void
+type ShouldStopCallback = () => boolean
+
+export const range = (size: number, startAt = 0) => {
+  return Array.from({ length: size }, (_, key) => key + startAt)
+}
+
+export const getRepoUrl = (user: string, repo: string) => {
+  return repoUrlTemplate.replace("{user}", user).replace("{repo}", repo)
+}
+
+export const getStorage = (): Storage => {
+  if (sessionStorage.getItem(storageKey)) {
+    return sessionStorage
+  } else if (localStorage.getItem(storageKey)) {
+    return localStorage
+  } else {
+    return sessionStorage
+  }
+}
+
+export const getStorageType = () => {
+  switch (exports.getStorage()) {
+    case sessionStorage:
+      return StorageType.SessionStorage
+    case localStorage:
+      return StorageType.LocalStorage
+    default:
+      return null
+  }
+}
+
+export const setStorageType = (storageType: StorageType) => {
+  removeAccessToken()
+
+  return storageType == StorageType.LocalStorage ? localStorage : sessionStorage
+}
+
+export const getAccessToken = (): string | null => {
+  return exports.getStorage().getItem(storageKey)
+}
+
+export const removeAccessToken = () => {
+  exports.getStorage().removeItem(storageKey)
+}
+
+export const isLoggedIn = () => {
+  return getAccessToken() !== null
+}
+
+export const prepareRequestHeaders = (accessToken: string | null) => {
+  return {
+    headers: {
+      Accept: "application/vnd.github.v3.star+json",
+      Authorization: accessToken ? "token " + accessToken : "",
+    },
+  }
+}
+
+export const validateAndStoreAccessToken = async (
+  accessToken: string,
+  storageType: StorageType,
+) => {
+  try {
+    await axios.get(validateAccessTokenURL, exports.prepareRequestHeaders(accessToken))
+    exports.setStorageType(storageType).setItem(storageKey, accessToken)
+    return true
+  } catch {
+    return false
+  }
+}
+
+export const loadStarGazerPage = async (
+  user: string,
+  repo: string,
+  pageNum: number,
+): Promise<AxiosResponse> => {
+  const accessToken = exports.getAccessToken()
+  return await axios.get(
+    stargazersURL
+      .replace("{user}", user)
+      .replace("{repo}", repo)
+      .replace("{page}", pageNum.toString()),
+    exports.prepareRequestHeaders(accessToken),
+  )
+}
+
+export const addStarData = (
+  starData: Array<InternalStarData>,
+  starCount: number,
+  pageData: Array<GitHubStarData>,
+) => {
+  for (let i = 0; i < pageData.length; i++) {
+    starData.push({
+      x: pageData[i].starred_at,
+      y: ++starCount,
+    })
+  }
+
+  return starCount
+}
+
+export const getLastStargazerPage = (linkHeader?: string) => {
+  if (!linkHeader || linkHeader.length === 0) {
+    return 1
+  }
+
+  // Split parts by comma
+  const parts = linkHeader.split(",")
+
+  // Parse each part into a named link
+  for (const i in parts) {
+    const section = parts[i].split(";")
+    if (section.length !== 2) {
+      continue
+    }
+
+    const url = section[0].replace(/<(.*)>/, "$1").trim()
+    const name = section[1].replace(/rel="(.*)"/, "$1").trim()
+
+    // if name is 'last' then extract page and return it
+    if (name === "last") {
+      return parseInt(url.replace(/(.*)&page=(.*)/, "$2").trim())
+    }
+  }
+  throw new Error("Cannot find last stargazer page")
+}
+
+export const loadStargazers = async (
+  user: string,
+  repo: string,
+  handleProgress: HandleProgressCallback,
+  shouldStop: ShouldStopCallback,
+) => {
+  try {
+    const starData: Array<InternalStarData> = []
+    let starCount = 0
+
+    handleProgress(0)
+
+    const page = await loadStarGazerPage(user, repo, 1)
+    const numOfPages = getLastStargazerPage(page.headers.link)
+    if (numOfPages > maxSupportedPagesWithoutAccessToken && !isLoggedIn()) {
+      throw Error(
+        "Cannot load a repo with more than " +
+          100 * maxSupportedPagesWithoutAccessToken +
+          ' stars without GitHub access token. Please click "GitHub Authentication" and provide one',
+      )
+    }
+
+    starCount = addStarData(starData, starCount, page.data)
+
+    handleProgress((1 / numOfPages) * 100)
+    let pageNum = 2
+    while (pageNum <= numOfPages) {
+      if (shouldStop()) {
+        return null
+      }
+
+      const currentBatchSize = Math.min(pageNum + batchSize, numOfPages + 1) - pageNum
+      const pages = await Promise.all(
+        range(currentBatchSize, pageNum).map((num) => loadStarGazerPage(user, repo, num)),
+      )
+      for (let i = 0; i < currentBatchSize; i++) {
+        starCount = addStarData(starData, starCount, pages[i].data)
+      }
+
+      pageNum += currentBatchSize
+      handleProgress(((pageNum - 1) / numOfPages) * 100)
+    }
+
+    return starData
+  } catch (error) {
+    if (!axios.isAxiosError(error)) {
+      throw error
+    }
+    if (!error.response) {
+      throw error
+    }
+    switch (error.response.status) {
+      case 404: {
+        throw Error(`Repo ${user}/${repo} Not found`)
+      }
+      case 403: {
+        throw Error(
+          "API rate limit exceeded!" +
+            (isLoggedIn()
+              ? ""
+              : /* eslint quotes: ["error", "double", { "avoidEscape": true }] */
+                ' Please click "GitHub Authentication" and provide GitHub access token to increase rate limit'),
+        )
+      }
+      default: {
+        throw Error(
+          "Couldn't fetch stargazers data, error code " +
+            error.response.status +
+            " returned" +
+            error.response.data?.message,
+        )
+      }
+    }
+  }
+}
