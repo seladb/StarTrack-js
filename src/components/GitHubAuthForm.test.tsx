bf83fd4343d9f5a186e73bcb9e876058c215e797
seladb <pcapplusplus@gmail.com>
2023-02-13 01:47:26 -0800

    Add GitHubAuthForm

---
+++ src/components/GitHubAuthForm.test.tsx
@@ -0,0 +1,122 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react"
+import GitHubAuthForm from "./GitHubAuthForm"
+import * as GitHubUtils from "../utils/GitHubUtils"
+
+describe(GitHubAuthForm, () => {
+  const onClose = jest.fn()
+  const accessTokenValue = "access_token"
+
+  const setup = (isTokenValid: boolean) => {
+    const validateAndStoreAccessTokenMock = jest
+      .spyOn(GitHubUtils, "validateAndStoreAccessToken")
+      .mockReturnValue(Promise.resolve(isTokenValid))
+
+    render(<GitHubAuthForm open={true} onClose={onClose} />)
+
+    const textBox = screen.getByRole("textbox") as HTMLInputElement
+    const loginBtn = screen.getByRole("button", { name: "Login" })
+    const closeBtn = screen.getByRole("button", { name: "Close" })
+    const checkBox = screen.getByRole("checkbox")
+
+    return {
+      textBox: textBox,
+      loginBtn: loginBtn,
+      closeBtn: closeBtn,
+      checkBox: checkBox,
+      validateAndStoreAccessTokenMock: validateAndStoreAccessTokenMock,
+    }
+  }
+
+  it("Handles valid access token", async () => {
+    const { textBox, loginBtn, validateAndStoreAccessTokenMock } = setup(true)
+
+    fireEvent.change(textBox, { target: { value: accessTokenValue } })
+    fireEvent.click(loginBtn)
+
+    await waitFor(() => {
+      expect(validateAndStoreAccessTokenMock).toHaveBeenCalledWith(
+        accessTokenValue,
+        GitHubUtils.StorageType.SessionStorage,
+      )
+      expect(onClose).toHaveBeenCalledWith(accessTokenValue)
+      expect(textBox.value).toBe("")
+    })
+  })
+
+  it("Handles valid access token and save in local storage", async () => {
+    const { textBox, loginBtn, checkBox, validateAndStoreAccessTokenMock } = setup(true)
+
+    fireEvent.change(textBox, { target: { value: accessTokenValue } })
+    fireEvent.click(checkBox)
+    fireEvent.click(loginBtn)
+
+    await waitFor(() => {
+      expect(validateAndStoreAccessTokenMock).toHaveBeenCalledWith(
+        accessTokenValue,
+        GitHubUtils.StorageType.LocalStorage,
+      )
+      expect(onClose).toHaveBeenCalledWith(accessTokenValue)
+      expect(textBox.value).toBe("")
+    })
+  })
+
+  it("Cleanup when close", async () => {
+    const { textBox, closeBtn, validateAndStoreAccessTokenMock } = setup(true)
+
+    fireEvent.change(textBox, { target: { value: accessTokenValue } })
+    fireEvent.click(closeBtn)
+
+    await waitFor(() => {
+      expect(validateAndStoreAccessTokenMock).not.toHaveBeenCalled()
+      expect(onClose).toHaveBeenCalledWith(null)
+      expect(textBox.value).toBe("")
+    })
+  })
+
+  it("Handles missing token", async () => {
+    const { loginBtn, validateAndStoreAccessTokenMock } = setup(true)
+
+    fireEvent.click(loginBtn)
+
+    await waitFor(() => {
+      expect(screen.getByText("Access token is empty or invalid.")).toBeInTheDocument()
+      expect(onClose).not.toHaveBeenCalled()
+      expect(validateAndStoreAccessTokenMock).not.toHaveBeenCalled()
+    })
+  })
+
+  it("Handles invalid token", async () => {
+    const { textBox, loginBtn, validateAndStoreAccessTokenMock } = setup(false)
+
+    fireEvent.change(textBox, { target: { value: accessTokenValue } })
+    fireEvent.click(loginBtn)
+
+    await waitFor(() => {
+      expect(validateAndStoreAccessTokenMock).toHaveBeenCalledWith(
+        accessTokenValue,
+        GitHubUtils.StorageType.SessionStorage,
+      )
+      expect(screen.getByText("Access token is empty or invalid.")).toBeInTheDocument()
+      expect(onClose).not.toHaveBeenCalled()
+    })
+  })
+
+  it("Cleanup when close after invalid token", async () => {
+    const { textBox, loginBtn, closeBtn } = setup(true)
+
+    fireEvent.click(loginBtn)
+
+    await waitFor(() => {
+      expect(screen.getByText("Access token is empty or invalid.")).toBeInTheDocument()
+      expect(onClose).not.toHaveBeenCalledWith(null)
+    })
+
+    fireEvent.click(closeBtn)
+
+    await waitFor(() => {
+      expect(screen.getByText("These credentials aren't stored in any server.")).toBeInTheDocument()
+      expect(onClose).toHaveBeenCalledWith(null)
+      expect(textBox.value).toBe("")
+    })
+  })
+})
---
+++ src/components/GitHubAuthForm.tsx
@@ -0,0 +1,129 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+} from "@mui/material"
+import Checkbox from "@mui/material/Checkbox"
+import FormControlLabel from "@mui/material/FormControlLabel"
+import React from "react"
+import { StorageType, validateAndStoreAccessToken } from "../utils/GitHubUtils"
+
+enum TokenValidationStatus {
+  Init = "init",
+  Valid = "valid",
+  Invalid = "invalid",
+}
+
+interface GitHubAuthFormProps {
+  open: boolean
+  onClose: (accessToken: string | null) => void
+}
+
+export default function GitHubAuthForm({ open, onClose }: GitHubAuthFormProps) {
+  const [accessTokenValue, setAccessTokenValue] = React.useState<string | null>(null)
+  const [accessTokenValid, setAccessTokenValid] = React.useState<TokenValidationStatus>(
+    TokenValidationStatus.Init,
+  )
+  const [storageType, setStorageType] = React.useState<StorageType>(StorageType.SessionStorage)
+
+  const handleStorageTypeCheckChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setStorageType(event.target.checked ? StorageType.LocalStorage : StorageType.SessionStorage)
+  }
+
+  const handleLoginClick = async () => {
+    if (accessTokenValue) {
+      const isValid = await validateAndStoreAccessToken(accessTokenValue, storageType)
+      isValid
+        ? setAccessTokenValid(TokenValidationStatus.Valid)
+        : setAccessTokenValid(TokenValidationStatus.Invalid)
+    } else {
+      setAccessTokenValid(TokenValidationStatus.Invalid)
+    }
+  }
+
+  const handleClose = () => {
+    const accessToken = accessTokenValid === TokenValidationStatus.Valid ? accessTokenValue : null
+    setAccessTokenValid(TokenValidationStatus.Init)
+    setAccessTokenValue(null)
+    onClose(accessToken)
+  }
+
+  const textFieldHelperText = () => {
+    return accessTokenValid === TokenValidationStatus.Invalid
+      ? "Access token is empty or invalid."
+      : "These credentials aren't stored in any server."
+  }
+
+  React.useEffect(() => {
+    if (accessTokenValid === TokenValidationStatus.Valid) {
+      handleClose()
+    }
+  }, [accessTokenValid])
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>GitHub Authentication</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          <p>
+            GitHub API{" "}
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://developer.github.com/v3/#rate-limiting'
+            >
+              rate limiter
+            </a>{" "}
+            makes it impossible to collect stargazer data on repos with more than 3000 stars without
+            GitHub authentication.
+          </p>
+          <p>
+            If you&apos;d like to view stargazer data for this repo, please provide your GitHub auth
+            details.
+          </p>
+          <p>
+            Please note these credentials aren&apos;t stored in any server. This application is
+            based on pure javascript so the credentials are only used to send authenticated requests
+            to GitHub API.
+          </p>
+          <p>
+            You can generate an access token{" "}
+            <a target='_blank' rel='noopener noreferrer' href='https://github.com/settings/tokens'>
+              here
+            </a>
+            .
+          </p>
+        </DialogContentText>
+        <TextField
+          autoFocus
+          margin='dense'
+          id='name'
+          label='GitHub Access Token'
+          value={accessTokenValue || ""}
+          fullWidth
+          variant='standard'
+          required
+          error={accessTokenValid === TokenValidationStatus.Invalid}
+          helperText={textFieldHelperText()}
+          onChange={(e) => {
+            setAccessTokenValue(e.target.value)
+          }}
+        />
+        <FormControlLabel
+          control={<Checkbox onChange={handleStorageTypeCheckChanged} />}
+          label='Save access token in local storage'
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Close</Button>
+        <Button onClick={handleLoginClick} autoFocus>
+          Login
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
