0bd8d135edac794bfee8ec83887c5b041ac9e4da
seladb <pcapplusplus@gmail.com>
2023-04-15 23:06:15 -0700

    Add AlertContext

---
+++ src/shared/AlertContext.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { AlertContextProvider, useAlertDialog } from "./AlertContext";
+import { Button } from "@mui/material";
+
+const TestComponent = () => {
+  const { showAlert } = useAlertDialog();
+
+  const handleClick = () => {
+    showAlert("Error occurred!");
+  };
+
+  return <Button onClick={handleClick}></Button>;
+};
+describe("Alert context", () => {
+  it("shows an alert", async () => {
+    render(
+      <AlertContextProvider>
+        <TestComponent />
+      </AlertContextProvider>,
+    );
+
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+
+    expect(screen.getByText("Error occurred!")).toBeInTheDocument();
+
+    const closeIcon = screen.getByTestId("CloseIcon");
+    fireEvent.click(closeIcon);
+
+    await waitFor(() => {
+      expect(screen.queryByText("Error occurred!")).not.toBeInTheDocument();
+    });
+  });
+
+  it("throws an error if AlertContextProvider doesn't exist", () => {
+    expect(() => render(<TestComponent />)).toThrow(
+      "useAlertDialog must be used within an AlertContextProvider",
+    );
+  });
+});
---
+++ src/shared/AlertContext.tsx
@@ -0,0 +1,55 @@
+import Snackbar from "@mui/material/Snackbar";
+import { Alert } from "@mui/lab";
+import React, { createContext, useContext } from "react";
+
+type AlertDialogContextActions = {
+  showAlert: (text: string) => void;
+};
+
+const AlertDialogContext = createContext({} as AlertDialogContextActions);
+
+interface AlertContextProviderProps {
+  children: React.ReactNode;
+}
+
+const AlertContextProvider: React.FC<AlertContextProviderProps> = ({ children }) => {
+  const [open, setOpen] = React.useState<boolean>(false);
+  const [message, setMessage] = React.useState<string>("");
+
+  const showAlert = (text: string) => {
+    setMessage(text);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <AlertDialogContext.Provider value={{ showAlert }}>
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity="error" variant="filled">
+          {message}
+        </Alert>
+      </Snackbar>
+      {children}
+    </AlertDialogContext.Provider>
+  );
+};
+
+const useAlertDialog = (): AlertDialogContextActions => {
+  const context = useContext(AlertDialogContext);
+
+  if (!context || Object.keys(context).length === 0) {
+    throw new Error("useAlertDialog must be used within an AlertContextProvider");
+  }
+
+  return context;
+};
+
+export { AlertContextProvider, useAlertDialog };
