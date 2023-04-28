2b47073da812ff22ea6297ac128ec563e0d606b9
seladb <pcapplusplus@gmail.com>
2023-04-28 02:25:14 -0700

    Remove App.test.tsx

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders learn react link", () => {
-  render(<App />);
-  // TODO
-});
