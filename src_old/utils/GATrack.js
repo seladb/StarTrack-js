70f71ba7d67ce6fb56c08e5490c754349ea23af0
seladb <pcapplusplus@gmail.com>
2024-02-14 01:27:26 -0800

    Delete the old source code

--- src_old/utils/GATrack.js
+++
@@ -1,8 +0,0 @@
-import ReactGA from "react-ga";
-
-function initializeGoogleAnalytics() {
-  ReactGA.initialize("UA-104097715-1");
-  ReactGA.pageview(window.location.pathname + window.location.search);
-}
-
-export default initializeGoogleAnalytics;
