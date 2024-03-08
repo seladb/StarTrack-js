6cd8bb54b00fa86579b31a6873f2497cae00cce1
seladb <pcapplusplus@gmail.com>
2024-03-08 00:31:57 -0800

    Add download zip to file utils

--- src/utils/FileUtils.ts
+++ src/utils/FileUtils.ts
@@ -1,5 +1,7 @@
-export const downloadFile = (fileName: string, fileContent: string, contentType: string) => {
-  const blob = new Blob([fileContent], { type: contentType });
+import { downloadZip } from "client-zip";
+import * as FileUtils from "./FileUtils";
+
+export const downloadFileFromBlob = (fileName: string, blob: Blob) => {
   const objectUrl = URL.createObjectURL(blob);
   try {
     const anchorEl = document.createElement("a");
@@ -9,4 +11,26 @@ export const downloadFile = (fileName: string, fileContent: string, contentType:
   } finally {
     URL.revokeObjectURL(objectUrl);
   }
+};
+
+export const downloadFile = (fileName: string, fileContent: string, contentType: string) => {
+  const blob = new Blob([fileContent], { type: contentType });
+  return FileUtils.downloadFileFromBlob(fileName, blob);
+};
+
+type FileInfo = {
+  name: string;
+  content: string;
+};
+
+export const zipAndDownloadFiles = async (files: Array<FileInfo>, zipFileName: string) => {
+  const fileInfos = files.map(({ name, content }) => {
+    return {
+      name: name,
+      input: content,
+    };
+  });
+
+  const blob = await downloadZip(fileInfos).blob();
+  FileUtils.downloadFileFromBlob(zipFileName, blob);
 };
