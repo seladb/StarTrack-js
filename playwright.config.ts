0b195925c73f0c1d7546702fd1d1ee0c76d05f04
seladb <pcapplusplus@gmail.com>
2024-12-09 01:05:15 -0800

    Add screenshots for debugging

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,6 +32,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+
+    screenshot: "on",
   },
 
   /* Configure projects for major browsers */
