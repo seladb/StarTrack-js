3c8b1cf7ffb41dc1dd2dde8ffa7bb40855b97717
seladb <pcapplusplus@gmail.com>
2025-02-02 18:54:03 -0800

    Change `react-router-dom` -> `react-router` and fix tests

--- src/routes/Preload/Preload.tsx
+++ src/routes/Preload/Preload.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router";
 import { ProgressProvider } from "../../shared/ProgressContext";
 import RepoLoader from "./RepoLoader";
 import { RepoMetadata } from "./PreloadTypes";
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+import { TextEncoder, TextDecoder } from "node:util";
+
+if (!global.TextEncoder) {
+  global.TextEncoder = TextEncoder;
+}
+
+if (!global.TextDecoder) {
+  global.TextDecoder = TextDecoder;
+}
