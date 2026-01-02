79479b2abc35db7f6f51e798e708593af0c84705
seladb <pcapplusplus@gmail.com>
2026-01-02 02:54:28 -0800

    Fix formatting

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -2,14 +2,12 @@ import type { Metric } from "web-vitals";
 
 const reportWebVitals = (onPerfEntry?: (metric: Metric) => void) => {
   if (onPerfEntry && typeof onPerfEntry === "function") {
-    import("web-vitals").then(
-      ({ onCLS, onFCP, onLCP, onTTFB }) => {
-        onCLS(onPerfEntry);
-        onFCP(onPerfEntry);
-        onLCP(onPerfEntry);
-        onTTFB(onPerfEntry);
-      }
-    );
+    import("web-vitals").then(({ onCLS, onFCP, onLCP, onTTFB }) => {
+      onCLS(onPerfEntry);
+      onFCP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
+    });
   }
 };
 
