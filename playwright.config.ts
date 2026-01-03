59c67ef035f65cf646d0e5bfe74082cce418bc56
seladb <pcapplusplus@gmail.com>
2026-01-03 01:14:01 -0800

    Add playwright server logs

--- playwright.config.ts
+++ playwright.config.ts
@@ -77,5 +77,7 @@ export default defineConfig({
     command: "yarn run start",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
+    stdout: "pipe",
+    stderr: "pipe",
   },
 });
